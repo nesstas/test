7ba66f8e10af55300eca5ca38b8e536333a9db13
Stanislav Nesterenkov <nesstas@mail.ru>
2016-01-02 23:45:47 +0300

    Добавил html

--- pass.php
+++ pass.php
@@ -3,6 +3,20 @@
 $pass="prixlop30";
 echo md5(md5($pass).'7em');
 echo "test";
-
+ 
 
 ?>
+<ul>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+	<li></li>
+</ul>
+<ul>
+	<li class="item5"></li>
+	<li class="item4"></li>
+	<li class="item3"></li>
+	<li class="item2"></li>
+	<li class="item1"></li>
+</ul>
